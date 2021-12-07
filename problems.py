845a63244a9d6c6a28344bf1e2940046bda4e999
Austin Luu <32989647+AustinLuu@users.noreply.github.com>
2021-12-07 16:10:15 -0500

    new problem solutions

--- problems.py
+++ problems.py
@@ -49,3 +49,82 @@ class Solution:
                     return currentPrefix
             currentPrefix += strs[0][i]
             i += 1
+#20. Valid Parentheses
+class Solution(object):
+    def isValid(self, s):
+        while "()" in s or "{}" in s or '[]' in s:
+            s = s.replace("()", "").replace('{}', "").replace('[]', "")
+        return s == ''
+#21. Merge Two Sorted Lists
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        prehead = ListNode(-1)
+        prev = prehead
+        
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+                #print(l1)
+            else:
+                prev.next = l2
+                l2 = l2.next
+                #print(l2)
+            prev = prev.next
+
+        # At least one of l1 and l2 can still have nodes at this point, so connect
+        # the non-null list to the end of the merged list.
+        prev.next = l1 if l1 is not None else l2
+        # print(prehead)
+
+        return prehead.next
+# 26. Remove Duplicates from Sorted Array
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+        if len(nums) == 0:
+            return 0
+        temp = nums[0]
+        while i < len(nums):
+            if nums[i]==temp:
+                del nums[i]
+            else:
+                temp = nums[i]
+                i+=1
+        print(nums)
+#27. Remove Element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        while count is not -1:
+            try:
+                nums.remove(val)
+                count += 1
+            except:
+                break
+#28. Implement strStr()
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        k = len(needle)
+        l = 0
+        if k == 0:
+            return 0
+        while l <= (len(haystack)-k):
+            if haystack[l:l+k] == needle:
+                return l
+            l+=1
+        return -1
+#35. Search Insert Position
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = (left+right)//2
+            if nums[pivot] == target:
+                return pivot
+            if target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return left
