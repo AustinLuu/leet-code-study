50918e9ef2cada6f4e72c57647830880a4d8a036
Austin Luu <32989647+AustinLuu@users.noreply.github.com>
2021-12-12 19:06:16 -0500

    new questions

--- problems.py
+++ problems.py
@@ -251,3 +251,46 @@ class Solution:
         
         # Sort nums1 list in-place.
         nums1.sort()
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         """
+#         # Make a copy of the first m elements of nums1.
+#         nums1_copy = nums1[:m] 
+        
+#         # Read pointers for nums1Copy and nums2 respectively.
+#         p1 = 0
+#         p2 = 0
+        
+#         # Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+#         for p in range(n + m):
+#             # We also need to ensure that p1 and p2 aren't over the boundaries
+#             # of their respective arrays.
+#             if p2 >= n or (p1 < m and nums1_copy[p1] <= nums2[p2]):
+#                 nums1[p] = nums1_copy[p1] 
+#                 p1 += 1
+#             else:
+#                 nums1[p] = nums2[p2]
+#                 p2 += 1
+#94. Binary Tree Inorder Traversal
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def df(node):
+            if node is None:
+                return
+            else: 
+                if node.left:
+                    df(node.left)
+                ans.append(node.val)
+                if node.right:
+                    df(node.right)
+        df(root)
+        return ans
